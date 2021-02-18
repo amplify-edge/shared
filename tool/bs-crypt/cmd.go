3575b89fdbd5298c2a656ef2e796287d435f83c9
alexadhy <alex.dhyatma@ubuntusoftware.net>
2021-02-18 12:27:02 +0000

    separate cmd to its own sub package

--- tool/bs-crypt/cmd.go
+++ tool/bs-crypt/cmd.go
@@ -1,27 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"go.amplifyedge.org/shared-v2/tool/bs-crypt/cmd"
 	"log"
-	"os"
 
-	"go.amplifyedge.org/shared-v2/tool/bs-crypt/lib"
 	"github.com/spf13/cobra"
 )
 
 const (
-	defaultSrcDir = "./plain"
-	defaultDstDir = "./encrypted"
-	defaultUsage  = "bs-crypt <encrypt | decrypt> --src <SRCPATH> --dest <DESTPATH>"
-	commandName   = "bs-crypt"
-
-	defaultEncryptUsage = "encrypt -s <SRC> -d <DEST>"
-	defaultDecryptUsage = "decrypt -s <SRC> -d <DEST>"
-)
-
-var (
-	srcDir string
-	dstDir string
+	defaultUsage = "bs-crypt <encrypt | decrypt> --src <SRCPATH> --dest <DESTPATH>"
+	commandName  = "bs-crypt"
 )
 
 func main() {
@@ -30,42 +18,8 @@ func main() {
 		Short: defaultUsage,
 		Long:  defaultUsage,
 	}
-	rootCmd.AddCommand(encryptCmd(), decryptCmd())
+	rootCmd.AddCommand(cmd.EncryptCmd(), cmd.DecryptCmd())
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func encryptCmd() *cobra.Command {
-	rootCmd := &cobra.Command{
-		Use:   defaultEncryptUsage,
-		Short: defaultEncryptUsage,
-	}
-	rootCmd.PersistentFlags().StringVarP(&srcDir, "src", "s", defaultSrcDir, "source directory for encryption / decryption")
-	rootCmd.PersistentFlags().StringVarP(&dstDir, "dest", "d", defaultDstDir, "destination directory for encryption / decryption")
-	rootCmd.RunE = func(cmd *cobra.Command, args []string) error {
-		passwd := os.Getenv("BS_CRYPT_PASSWORD")
-		if passwd == "" {
-			return fmt.Errorf("BS_CRYPT_PASSWORD is empty")
-		}
-		return lib.EncryptAllFiles(srcDir, dstDir, passwd)
-	}
-	return rootCmd
-}
-
-func decryptCmd() *cobra.Command {
-	rootCmd := &cobra.Command{
-		Use:   defaultDecryptUsage,
-		Short: defaultDecryptUsage,
-	}
-	rootCmd.PersistentFlags().StringVarP(&srcDir, "src", "s", defaultSrcDir, "source directory for encryption / decryption")
-	rootCmd.PersistentFlags().StringVarP(&dstDir, "dest", "d", defaultDstDir, "destination directory for encryption / decryption")
-	rootCmd.RunE = func(cmd *cobra.Command, args []string) error {
-		passwd := os.Getenv("BS_CRYPT_PASSWORD")
-		if passwd == "" {
-			return fmt.Errorf("BS_CRYPT_PASSWORD is empty")
-		}
-		return lib.DecryptAllFiles(srcDir, dstDir, passwd)
-	}
-	return rootCmd
 }
